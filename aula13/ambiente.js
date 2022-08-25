f4773004b24114401a514d2df2e1410087c5ae50
Christian Ronald <110695050+chronald@users.noreply.github.com>
2022-08-25 19:05:52 -0300

    Repetições;

---
+++ aula13/ambiente.js
@@ -0,0 +1,16 @@
+// Aula sobre repetições (part1)
+
+// Estrutura de repetição com teste lógico no inicio
+while (c <= 5) {
+    console.log(`Passo ${c}`)
+    c++ 
+} console.log('finish¹')
+
+// Estrutura de repetição com teste lógico no final
+var cc = 1
+do {
+    console.log(`Passo ${cc}`)
+    cc++
+} while (cc <= 6)
+console.log('finish²')
+
