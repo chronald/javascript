d4c30eb70cd2f73264816b1fe46420462811e5be
Christian Ronald <110695050+chronald@users.noreply.github.com>
2022-08-29 20:56:59 -0300

    Update script.js

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,24 @@
-alert('Olá')
+function validar() {
+    var num = document.getElementById('txttab')
+    var tab = document.getElementById('seltab')
+
+    if (num.value.length == 0){
+        alert('Digite um número.')
+    
+    } else {
+        var r = Number(num)
+        var c = 1 
+        tab.innerHTML = ''
+        while ( c <= 10 )
+        var item = document.createElement('option')
+        item.text = `${r} x ${c} = ${r*c}`
+        text.innerHTML = `tab${c}`
+        tab.appendChild(item)
+        c++
+    }
+
+
+}
+
+
+
